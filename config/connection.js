a91684ebb7417bebd3ded0cfe6dab12a9d389620
Amanda Ogonosky <amandaogonosky@Os-MacBook-Air.local>
2018-05-13 21:02:38 -0600

    update with heroku BS

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,30 @@
 // mysql connect
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgah_db"
+if (process.env.JAWSDB_URL){
+connection =mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+connection =mysql.createConnection({
+host: "localhost",
+user: "root",
+password: "root",
+database: "burgah_db"
+
 });
+
+}
+
+
+// mysql://dnph00ip4p215c6e:yf5px5uqy42804n4@s0znzigqvfehvff5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/uv9i2vivkcol1gdw
+
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "burgah_db"
+// });
 
 // connect to the database with error handeling
 connection.connect(function(err) {
